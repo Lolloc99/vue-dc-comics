76bc32ce134f575360df09e6a8af0c2287497c17
Lolloc99 <lolloc99@yahoo.it>
2022-05-17 17:05:59 +0200

    Main aggiunto

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,5 +1,9 @@
 <template>
-  <main></main>
+  <main class="main">
+    <div class="main-content container">
+      <h1>Content goes here</h1>
+    </div>
+  </main>
 </template>
 
 <script>
@@ -8,4 +12,26 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+@import "../style/common.scss";
+
+.main {
+  width: 100%;
+  background-color: #1c1c1c;
+
+  /* test */
+  height: 130px;
+}
+
+.main-content {
+  display: flex;
+  align-items: center;
+
+  /* test */
+  height: 130px;
+}
+
+h1 {
+  color: white;
+}
+</style>
