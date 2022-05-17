48ca1091066334a834ea48f9c18ae7e0f4b0855c
Lolloc99 <lolloc99@yahoo.it>
2022-05-17 16:53:01 +0200

    Header aggiunto

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,14 +1,16 @@
 <template>
-  <header class="header container">
-    <div>
-      <img src="../assets/img/dc-logo.png" alt="dc-logo." />
-    </div>
-    <div>
-      <ul>
-        <li>prova</li>
-        <li>prova</li>
-        <li>prova</li>
-      </ul>
+  <header class="header">
+    <div class="header-content container">
+      <div class="header-img">
+        <img src="../assets/img/dc-logo.png" alt="dc-logo." />
+      </div>
+      <div>
+        <ul>
+          <li v-for="(item, index) in HeaderLinks" :key="index">
+            {{ item.text }}
+          </li>
+        </ul>
+      </div>
     </div>
   </header>
 </template>
@@ -16,65 +18,62 @@
 <script>
 export default {
   name: "AppHeader",
-    data: function () {
+  data: function () {
     return {
-        HeaderLinks: [
+      HeaderLinks: [
         {
-            url: "#",
-            text: "Characters",
-            active: false,
+          url: "#",
+          text: "Characters",
+          active: false,
         },
         {
-            url: "#",
-            text: "Comics",
-            active: true,
+          url: "#",
+          text: "Comics",
+          active: true,
         },
         {
-            url: "#",
-            text: "Movies",
-            active: false,
+          url: "#",
+          text: "Movies",
+          active: false,
         },
         {
-            url: "#",
-            text: "Tv",
-            active: false,
+          url: "#",
+          text: "Tv",
+          active: false,
         },
         {
-            url: "#",
-            text: "Games",
-            active: false,
+          url: "#",
+          text: "Games",
+          active: false,
         },
         {
-            url: "#",
-            text: "Collectibles",
-            active: false,
+          url: "#",
+          text: "Collectibles",
+          active: false,
         },
         {
-            url: "#",
-            text: "Videos",
-            active: false,
+          url: "#",
+          text: "Videos",
+          active: false,
         },
         {
-            url: "#",
-            text: "Fans",
-            active: false,
+          url: "#",
+          text: "Fans",
+          active: false,
         },
         {
-            url: "#",
-            text: "News",
-            active: false,
+          url: "#",
+          text: "News",
+          active: false,
         },
         {
-            url: "#",
-            text: "Shop",
-            active: false,
+          url: "#",
+          text: "Shop",
+          active: false,
         },
-        ],
+      ],
     };
   },
-  methods: function () {
-      return
-  }
 };
 </script>
 
@@ -82,8 +81,25 @@ export default {
 @import "../style/common.scss";
 
 .header {
+  width: 100%;
+}
+
+.header-content {
   display: flex;
   justify-content: space-between;
-  background-color: white;
+  align-items: center;
+
+  .header-img img {
+    display: inline-block;
+    width: 80%;
+  }
+}
+
+li {
+  font-weight: bold;
+  line-height: 100px;
+  margin: 0.7rem;
+  border-bottom: 5px solid transparent;
+  text-transform: uppercase;
 }
 </style>
